7b1ddb879b392a87475b689ce594196b62fa357d
quanth <hqtran@linagora.com>
2020-11-06 14:11:49 +0700

    JAMES-3400 Add user list command

---
+++ server/protocols/webadmin-cli/src/main/java/org/apache/james/cli/user/UserListCommand.java
@@ -0,0 +1,52 @@
+/******************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one     *
+ * or more contributor license agreements.  See the NOTICE file   *
+ * distributed with this work for additional information          *
+ * regarding copyright ownership.  The ASF licenses this file     *
+ * to you under the Apache License, Version 2.0 (the              *
+ * "License"); you may not use this file except in compliance     *
+ * with the License.  You may obtain a copy of the License at     *
+ *                                                                *
+ * http://www.apache.org/licenses/LICENSE-2.0                     *
+ *                                                                *
+ * Unless required by applicable law or agreed to in writing,     *
+ * software distributed under the License is distributed on an    *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY         *
+ * KIND, either express or implied.  See the License for the      *
+ * specific language governing permissions and limitations        *
+ * under the License.                                             *
+ ******************************************************************/
+
+package org.apache.james.cli.user;
+
+import java.util.concurrent.Callable;
+
+import org.apache.james.cli.WebAdminCli;
+import org.apache.james.httpclient.UserClient;
+
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+    name = "list",
+    description = "Show all users on the users list")
+public class UserListCommand implements Callable<Integer> {
+
+    @CommandLine.ParentCommand UserCommand userCommand;
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            UserClient userClient = Feign.builder()
+                .decoder(new JacksonDecoder())
+                .target(UserClient.class, userCommand.webAdminCli.jamesUrl + "/users");
+            userClient.getDomainList().forEach(userCommand.out::println);
+            return WebAdminCli.CLI_FINISHED_SUCCEED;
+        } catch (Exception e) {
+            e.printStackTrace(userCommand.err);
+            return WebAdminCli.CLI_FINISHED_FAILED;
+        }
+    }
+
+}
