6a688a004c017c515f53889189b0e57649863d87
Rene Cordier <rcordier@linagora.com>
2023-11-22 11:20:30 +0700

    fixup! JAMES-2586 Guice bindings and package renaming for domain postgres implementation

--- server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresDomainListModule.java
+++ server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresDomainListModule.java
@@ -19,14 +19,17 @@
 
 package org.apache.james.modules.data;
 
+import org.apache.james.backends.postgres.PostgresModule;
 import org.apache.james.domainlist.api.DomainList;
 import org.apache.james.domainlist.lib.DomainListConfiguration;
 import org.apache.james.domainlist.postgres.PostgresDomainList;
+import org.apache.james.domainlist.postgres.PostgresDomainModule;
 import org.apache.james.utils.InitializationOperation;
 import org.apache.james.utils.InitilizationOperationBuilder;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import com.google.inject.multibindings.Multibinder;
 import com.google.inject.multibindings.ProvidesIntoSet;
 
 public class PostgresDomainListModule extends AbstractModule {
@@ -34,6 +37,7 @@ public class PostgresDomainListModule extends AbstractModule {
     public void configure() {
         bind(PostgresDomainList.class).in(Scopes.SINGLETON);
         bind(DomainList.class).to(PostgresDomainList.class);
+        Multibinder.newSetBinder(binder(), PostgresModule.class).addBinding().toInstance(PostgresDomainModule.MODULE);
     }
 
     @ProvidesIntoSet
